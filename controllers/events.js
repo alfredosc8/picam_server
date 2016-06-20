bc6789f1ef19648dd190bf681546da3f342116c4
Giovanni Coia <g.coia@icloud.com>
2016-06-19 18:42:15 -0700

    returning HTTP status codes instead of updated lists

--- controllers/events.js
+++ controllers/events.js
@@ -24,7 +24,7 @@ exports.create = function(req, res) {
             if (err) {
                 res.send(err);
             }
-            returnLatestEventList(res);
+            res.sendStatus(200);
         });
 };
 
@@ -44,7 +44,7 @@ exports.delete = function(req, res) {
         if (err) {
             res.send(err);
         }
-        returnLatestEventList(res);
+        res.sendStatus(200);
     });
 };
 
