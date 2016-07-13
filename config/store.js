52f069106d418b5e92309925198b24e787ad402b
Giovanni Coia <g.coia@icloud.com>
2016-07-12 20:01:12 -0700

    starting work on video support

---
+++ config/store.js
@@ -0,0 +1,17 @@
+var path = require('path');
+var multer = require('multer');
+
+var storage =   multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, path.join(__dirname, '../public/uploads'));
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.fieldname + '-' + Date.now());
+  }
+});
+
+var upload = multer({ storage : storage }).fields([{ name: 'previewImage', maxCount: 1 }, { name: 'video', maxCount: 1 }]);
+
+module.exports = {
+    videoUpload: upload
+};
